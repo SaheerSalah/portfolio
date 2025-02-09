8bbfc7d60ad4e21ef85256509f89a2dd00778379
SaheerSalah <sou6sal@gmail.com>
2025-02-09 20:28:58 +0200

    about card

--- src/components/AboutCard/index.tsx
+++ src/components/AboutCard/index.tsx
@@ -1,1 +1,20 @@
+import React from "react";
+interface PropsAboutCard {
+  headerName?: string;
+  content: React.ReactNode;
+  AdditionalClass?: string;
+}
+const AboutCard = ({headerName, content, AdditionalClass }: PropsAboutCard) => {
+  return (
+    <div className="">
+      <div className={`text-gray-700 bg-white    p-4 duration-300 ${AdditionalClass}`}>
+        <h2 className="text-lg font-semibold text-gray-800 ">
+          {headerName}
+        </h2>
+        <div>{content}</div>
+      </div>
+    </div>
+  );
+};
 
+export default AboutCard;
